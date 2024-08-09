939bb7ba00ba37926c6c8625e04b9a3821209659
Wazeer <58399263+wazeerc@users.noreply.github.com>
2024-08-09 20:36:41 +0400

    Implement react context

--- src/components/FlagsGuessr.tsx
+++ src/components/FlagsGuessr.tsx
@@ -1,37 +1,32 @@
-import { countryNamesAndCode } from '../data/allCountries'
-import CountriesGrid from './CountriesGrid'
-import OptionsGrid, { NextButton } from './OptionsGrid'
+import { useFlagsContext } from '../Context';
+import CountriesGrid from './CountriesGrid';
+import OptionsGrid, { NextButton } from './OptionsGrid';
 
-import './styles/FlagsGuessr.css'
+import './styles/FlagsGuessr.css';
 
 const FlagsGuessr = () => {
-    const sessionCountry =
-        countryNamesAndCode[
-            Math.floor(Math.random() * countryNamesAndCode.length)
-        ]
+    const { handleNextFlag } = useFlagsContext();
 
     return (
-        <>
-            <div className="flags-guessr-container">
-                <main className="flags-guessr-main">
-                    <CountriesGrid sessionCountry={sessionCountry} />
-                    <OptionsGrid sessionCountryName={sessionCountry.name} />
-                    <NextButton nextFlag={() => window.location.reload()} />
-                </main>
-                <footer>
-                    Thanks for playing! Contribute to the&nbsp;
-                    <a
-                        href="https://github.com/wazeerc/FlagsGuessr"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        GitHub repository
-                    </a>
-                    🫡
-                </footer>
-            </div>
-        </>
-    )
-}
+        <div className="flags-guessr-container">
+            <main className="flags-guessr-main">
+                <CountriesGrid />
+                <OptionsGrid />
+                <NextButton nextFlag={handleNextFlag} />
+            </main>
+            <footer>
+                Thanks for playing! Contribute to the&nbsp;
+                <a
+                    href="https://github.com/wazeerc/FlagsGuessr"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    GitHub repository
+                </a>
+                🫡
+            </footer>
+        </div>
+    );
+};
 
-export default FlagsGuessr
+export default FlagsGuessr;
